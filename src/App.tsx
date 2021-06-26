eaea777da60d9b740db271acc8da1b7411eb042a
Richard Baldin <richardbaldin@gmail.com>
2021-06-26 01:07:43 -0300

    adiciona funcionalidades do Like, exclusão de perguntas e encerrar uma sala

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
 
 import { AuthContextṔrovider } from "./contexts/AuthContext";
 
@@ -11,9 +12,10 @@ function App() {
     <BrowserRouter>
       <AuthContextṔrovider>
         <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" exact component={NewRoom} />
-        <Route path="/rooms/:id" component={Room} />
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" exact component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextṔrovider>
     </BrowserRouter>
